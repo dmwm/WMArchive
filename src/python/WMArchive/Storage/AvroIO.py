f24ff650334653c9da97bfdc4467c6eb484c708c
Valentin Kuznetsov <vkuznet@gmail.com>
2015-12-09 13:45:16 -0500

    New storage IO class to handle writing avro files from input data stream

---
+++ src/python/WMArchive/Storage/AvroIO.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#pylint: disable=
+"""
+File       : AvroIO.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: WMArchive File storage module
+"""
+
+# futures
+from __future__ import print_function, division
+
+# system modules
+import os
+import json
+import gzip
+import itertools
+from types import GeneratorType
+
+# avro modules
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+# WMArchive modules
+from WMArchive.Storage.BaseIO import Storage
+from WMArchive.Utils.Utils import tstamp, wmaHash
+
+class AvroStorage(Storage):
+    "Storage based on Avro file based back-end"
+    def __init__(self, uri):
+        # avro uri: avro:/path/schema.avsc
+        schema = uri.replace('avroio:', '')
+        uripath, _ = schema.rsplit('/', 1)
+        if  not os.path.exists(schema):
+            raise Exception("No avro schema file found in provided uri: %s" % uri)
+        Storage.__init__(self, uripath)
+        print(tstamp('WMA AvroIO storage'), uri)
+        if  not os.path.exists(self.uri):
+            os.makedirs(self.uri)
+        self.schema = avro.schema.parse(open(schema).read())
+
+    def write(self, data):
+        "Write API"
+        fname = '%s/%s.avro.gz' % (self.uri, wmaHash(data))
+        print(tstamp('WMA AvroIO::write'), fname, data)
+        with gzip.open(fname, 'w') as ostream:
+            with DataFileWriter(ostream, DatumWriter(), self.schema) as writer:
+                if  isinstance(data, list) or isinstance(data, GeneratorType):
+                    for rec in data:
+                        writer.append(rec)
+                elif isinstance(data, dict):
+                    writer.append(data)
+#            writer = DataFileWriter(ostream, DatumWriter(), schema)
+#            writer.append(data)
+#            writer.close()
+
+    def read(self, query=None):
+        "Read API"
+        pass
