1abbc5f0d3c1d47e00bf319b3ecf81872806d9c9
Valentin Kuznetsov <vkuznet@gmail.com>
2016-01-14 08:43:47 -0500

    Add SON class to manipulate incoming/outgoing docs; Add dropdb, write_bulk methods; fixed write; add unittest

--- src/python/WMArchive/Storage/MongoIO.py
+++ src/python/WMArchive/Storage/MongoIO.py
@@ -12,30 +12,58 @@ from __future__ import print_function, division
 
 # system modules
 import itertools
+import traceback
 
 # Mongo modules
 from pymongo import MongoClient
 from pymongo.errors import InvalidDocument, InvalidOperation, DuplicateKeyError
+from pymongo.son_manipulator import SONManipulator
+from bson.son import SON
 
 # WMArchive modules
 from WMArchive.Storage.BaseIO import Storage
 from WMArchive.Utils.Regexp import PAT_UID
+from WMArchive.Utils.Utils import tstamp
+
+class WMASONManipulator(SONManipulator):
+    """WMArchive MongoDB SON manipulator"""
+    def __init__(self):
+        SONManipulator.__init__(self)
+
+    def transform_incoming(self, son, collection):
+        "Manipulate an incoming SON object"
+        if  self.will_copy():
+            return SON(son)
+        return son
+
+    def transform_outgoing(self, son, collection):
+        "Manipulate an outgoing SON object"
+        if  self.will_copy():
+            return SON(son)
+        if  isinstance(son, dict) and '_id' in son:
+            del son['_id']
+        return son
 
 class MongoStorage(Storage):
     "Storage based on MongoDB back-end"
-    def __init__(self, uri):
+    def __init__(self, uri, dbname='fwjr', collname='db', chunk_size=1000):
         "ctor with mongo uri: mongodb://host:port"
         Storage.__init__(self, uri)
-        self.client = MongoClient(uri)
-        self.coll = self.client['fwjr']['db']
-        self.chunk_size = 100
+        self.client = MongoClient(uri, w=1)
+        self.mdb = self.client[dbname]
+        self.mdb.add_son_manipulator(WMASONManipulator())
+        self.coll = self.mdb[collname]
+        print(tstamp('WMA INFO'), self.coll)
+        self.chunk_size = chunk_size
 
-    def write_in_bluk(self, data):
+    def write_bulk(self, data):
         "Bulk write API, return ids of stored documents"
         return self.write(data)
 
     def write(self, data):
         "Write API, return ids of stored documents"
+        if  not isinstance(data, list):
+            data = [data]
         wmaids = self.getids(data)
         try:
             while True:
@@ -49,6 +77,7 @@ class MongoStorage(Storage):
         except DuplicateKeyError as exp:
             pass
         except Exception as exp:
+            print(traceback.print_exc())
             print(tstamp('WMA WARNING'), 'Uncaught exception', str(exp))
         return wmaids
 
@@ -56,14 +85,11 @@ class MongoStorage(Storage):
         "Read API"
         if  not query:
             query = {}
-        docs = []
         if  isinstance(query, list):
             query = {'wmaid': {'$in': query}}
         elif  PAT_UID.match(str(query)):
             query = {'wmaid': query}
-        for rec in self.coll.find(query):
-            del rec['_id'] # internal MongoDB id
-            docs.append(rec)
+        docs = [r for r in self.coll.find(query)]
         return docs
 
     def update(self, ids, spec):
@@ -71,6 +97,12 @@ class MongoStorage(Storage):
         doc_query = {'wmaid' : {'$in': ids}}
         self.coll.update(doc_query, spec, multi=True)
 
-    def remove(self, spec):
+    def remove(self, spec=None):
         "Remove documents from MongoDB for given spec"
+        if  not spec:
+            spec = {}
         self.coll.remove(spec)
+
+    def dropdb(self, dbname):
+        "Remove given database from MongoDB"
+        self.client.drop_database(dbname)
