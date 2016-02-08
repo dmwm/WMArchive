cc14ccf80732f1ea6b86f2dd1eeaca6c6423a696
Valentin Kuznetsov <vkuznet@gmail.com>
2016-02-08 15:59:05 -0500

    Re-wrote example

--- src/python/WMArchive/PySpark/RecordReader.py
+++ src/python/WMArchive/PySpark/RecordReader.py
@@ -1,22 +1,43 @@
 """
-This is example how to write extract function to be used by
-WMArchive/Tools/myspark.py tool. User should perform all
-necessary actions with given set of records and return back
-desired results.
+This is example how to write mapper and reducer functions for
+WMArchive/Tools/myspark.py tool. User should perform all necessary actions with
+given set of records and return back desired results. Here our mapper process
+records from avro files and collect results into a single dictionary. The
+reducer will collect results from all mappers and return back aggregated
+information.
 """
 
-def extract(records):
+def mapper(records):
     """
-    Function to extract necessary information from record during spark
+    Function to extract necessary information from records during spark
     collect process. It will be called by RDD.collect() object within spark.
     """
     out = []
+    tot_cpu = tot_time = rsize = wsize = count = 0
     for rec in records:
-        # do something useful with records, so far we just
-        # put them into output, input records parameters is
-        # what spark will extract from avro files and pass
-        # to this function.
-        out.append(rec)
-    return out
+        if  not rec:
+            continue
+        perf = rec['steps']['cmsRun1']['performance']
+        cpu = perf['cpu']
+        storage = perf['storage']
+        tot_cpu += cpu['TotalJobCPU']
+        tot_time += cpu['TotalJobTime']
+        rsize += storage['readTotalMB']
+        wsize += storage['writeTotalMB']
+        count += 1
+    summary = {'cpu':tot_cpu, 'time':tot_time, 'rsize':rsize, 'wsize':wsize, 'docs':count}
+    return summary
+
+def reducer(records):
+    "Simpler reducer which collects all results from RDD.collect() records"
+    tot_cpu = tot_time = rsize = wsize = count = 0
+    for rec in records:
+        tot_cpu += rec['cpu']
+        tot_time += rec['time']
+        rsize += rec['rsize']
+        wsize += rec['wsize']
+        count += rec['docs']
+    summary = {'cpu':tot_cpu, 'time':tot_time, 'rsize':rsize, 'wsize':wsize, 'docs':count}
+    out.append(summary)
 
 
