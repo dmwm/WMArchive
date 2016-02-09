9c83d3ebc20192485b8e0cf99e98898450810736
Valentin Kuznetsov <vkuznet@gmail.com>
2016-02-09 08:39:36 -0500

    Fix regex YYYYMMDD and add new one for YYYY

--- src/python/WMArchive/Utils/Regexp.py
+++ src/python/WMArchive/Utils/Regexp.py
@@ -18,4 +18,5 @@ PAT_INFO = re.compile(r"^[0-9]$")
 PAT_UID = re.compile(r"^[a-z0-9]{32,32}$")
 
 # time patterns
-PAT_YYYYMMDD = re.compile(r'[0-2]0[0-9][0-9][0-1][0-9][0-3][0-9]')
+PAT_YYYYMMDD = re.compile(r'20[0-9][0-9][0-1][0-9][0-3][0-9]')
+PAT_YYYY = re.compile(r'20[0-9][0-9]')
