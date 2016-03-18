69473c0c9a842ae5621e12ed1b41e626fd7fd561
Valentin Kuznetsov <vkuznet@gmail.com>
2016-03-18 09:32:35 -0400

    Add logic how to treat spec data type

--- src/python/WMArchive/Service/Manager.py
+++ src/python/WMArchive/Service/Manager.py
@@ -133,26 +133,31 @@ class WMArchiveManager(object):
         Yield list of found documents or None.
         """
         # convert given spec into query suitable for sts/lts
-        try:
-            trange = spec.pop('timerange')
-        except KeyError:
-            data = []
-            print(tstamp("WMArchiveManager::read"), "timerange is not provided")
-            status = 'fail'
-            reason = 'No timerange is provided, please adjust your query spec'
-            result = {'input': {'spec': spec, 'fields': fields},
-                      'results': data, 'storage': self.mgr.stype,
-                      'status': status, 'reason': reason}
-            return result
+        if  isinstance(spec, dict):
+            try:
+                trange = spec.pop('timerange')
+            except KeyError:
+                data = []
+                print(tstamp("WMArchiveManager::read"), "timerange is not provided")
+                status = 'fail'
+                reason = 'No timerange is provided, please adjust your query spec'
+                result = {'input': {'spec': spec, 'fields': fields},
+                          'results': data, 'storage': self.mgr.stype,
+                          'status': status, 'reason': reason}
+                return result
 
-        # based on given time range define which manager
-        # we'll use for data look-up
-        mgr = self.sts
-        if  use_lts(trange):
-            mgr = self.lts
+            # based on given time range define which manager
+            # we'll use for data look-up
+            mgr = self.sts
+            if  use_lts(trange):
+                mgr = self.lts
 
-        # convert spec into WMArchive one
-        spec, fields = self.sconvert(mgr, spec, fields)
+            # convert spec into WMArchive one
+            spec, fields = self.sconvert(mgr, spec, fields)
+        else:
+            # if spec is a list, it means user look-up docs by wmaids
+            # they represents results of LTS data look-up
+            mgr = self.sts
         status = 'ok'
         reason = None
         try:
