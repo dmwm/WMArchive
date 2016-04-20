02ff2e9a6592a953f8d31e24fe1d6b60020ba199
Seangchan Ryu <sryu@fnal.gov>
2016-04-20 00:06:07 -0500

    clean up schema

--- src/python/WMArchive/Schemas/FWJRProduction.py
+++ src/python/WMArchive/Schemas/FWJRProduction.py
@@ -7,26 +7,26 @@ fwjr = \
                'jobtype': "Processing",
                'jobstate': "success", 
                'ts': 1456500229},
- 'LFNArray': ['/store/data/Run2011A/Cosmics/RAW/v1/000/160/960/E8099605-8853-E011-A848-0030487A18F2.root',
-              '/store/unmerged/CMSSW_7_0_0_pre11/Cosmics/ALCARECO/DtCalib-RECOCOSD_TaskChain_Data_pile_up_test-v1/00000/ECCFE421-08CB-E511-9F4C-02163E017804.root',
+ 'LFNArray': ['/store/test/blah.root',
+              '/store/unmerged/blah.root',
               '/lfn/fallbackfile.root', '/lfn/skipedfile.root'],
  'LFNArrayRef': ['fallbackFiles',
                  'outputLFNs',
                  'lfn',
                  'skippedFiles',
                  'inputLFNs'],
- 'PFNArray': ['root://eoscms.cern.ch//eos/cms/store/data/Run2011A/Cosmics/RAW/v1/000/160/960/E8099605-8853-E011-A848-0030487A18F2.root',
-              'root://eoscms.cern.ch//eos/cms/store/unmerged/CMSSW_7_0_0_pre11/Cosmics/ALCARECO/DtCalib-RECOCOSD_TaskChain_Data_pile_up_test-v1/00000/ECCFE421-08CB-E511-9F4C-02163E017804.root',
+ 'PFNArray': ['root://test.ch/blah.root',
+              'root://test.ch/blah2.root',
               ],
  'PFNArrayRef': ['inputPFNs', 'outputPFNs', 'pfn'],  # list of keys whose value is referencing fileArray index
  
  'steps': [{'name': 'cmsRun1',
-             #'analysis': {},
+             #'analysis': {}, 
              #'cleanup': {},
              #'logs': {},
              'errors': [
                    {
-                       "details": "An exception of category 'ExternalLHEProducer' occurred while\n   [0] Processing run: 1\n   [1] Running path 'lhe_step'\n   [2] Calling beginRun for module ExternalLHEProducer/'externalLHEProducer'\nException Message:\nChild failed with exit code 2.",
+                       "details": "Failed badly.",
                        "type": "Fatal Exception",
                        "exitCode": 8001
                    }
@@ -46,44 +46,36 @@ fwjr = \
                          'adler32': 'e503b8b9',
                          'applicationName': 'cmsRun',
                          'applicationVersion': 'CMSSW_7_0_0_pre11',
-                         #TODO change to empty string
                          'async_dest': '',
                          'branch_hash': 'c1e135af4ac2eb2b803bb6487be2c80f',
                          'catalog': '',
                          'cksum': '2641269665',
-                         'configURL': 'https://cmsweb.cern.ch/couchdb;;reqmgr_config_cache;;5f4811e9ccd63d563cd62572350f0db8',
+                         'configURL': 'https://couch.config.ch5f4811e9ccd63d563cd62572350f0db8',
                          'events': 0,
                          'globalTag': 'GR_R_62_V3::All',
                          'guid': 'ECCFE421-08CB-E511-9F4C-02163E017804',
                          'inputDataset': '/Cosmics/Run2011A-v1/RAW',
                          'inputLFNs': [0],
                          'inputPFNs': [0],
-                         #TODO change to empty string from None
                          'location': '',
                          'merged': False,
                          'module_label': 'ALCARECOStreamDtCalib',
                          'ouput_module_class': 'PoolOutputModule',
-                         'outputDataset': '/Cosmics/CMSSW_7_0_0_pre11-DtCalib-RECOCOSD_TaskChain_Data_pile_up_test-v1/ALCARECO',
-                         #TODO need to change thsi to list format
+                         'outputDataset': '/test/proc/ALCARECO',
                          'outputLFNs': [1],
                          'outputPFNs': [1],
-                         #TODO change to empty string from 'None'
                          'prep_id': '',
                          'processingStr': 'RECOCOSD_TaskChain_Data_pile_up_test',
                          'processingVer': 1,
                          'runs': [{'lumis': [164, 165],
                                    'runNumber': 160960}],
                          'size': 647376,
-                         #TODO remove this 
-                         #'user_dn': '',
-                         #'user_vogroup': 'DEFAULT',
-                         #'user_vorole': 'DEFAULT',
                          'validStatus': 'PRODUCTION',
                          "SEName": "srm-cms.cern.ch",
                          "PNN": "T2_CERN_CH",
                          "GUID": '',
                          "StageOutCommand": "srmv2-lcg"}],
-              'performance': {#"multicore": {},
+              'performance': {"multicore": {},
                   "storage": {
                     "readAveragekB": 77.8474891246,
                     "readCachePercentageOps": 0.0,
@@ -117,4 +109,4 @@ fwjr = \
             ],
 'fallbackFiles': [0],
 'skippedFiles': [1],
-'task': '/sryu_TaskChain_Data_wq_testt_160204_061048_5587/RECOCOSD'}
+'task': '/workflow_name/RECOCOSD'}
