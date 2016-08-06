c098c38a9c65c4ac17b48aaa6357119daab7a85a
Nils Fischer <n.fischer@viwid.com>
2016-08-06 17:54:59 +0200

    moved stats extraction and aggregation to standalone functions

--- src/python/WMArchive/PySpark/RecordAggregator.py
+++ src/python/WMArchive/PySpark/RecordAggregator.py
@@ -8,6 +8,7 @@ import json
 from datetime import datetime
 from pymongo import MongoClient
 from bson import json_util
+import time
 
 
 def get_scope_hash(scope):
@@ -17,10 +18,100 @@ def get_scope_hash(scope):
     return str(hash(frozenset(scope.items())))
 
 
+def extract_stats(record):
+
+    meta_data = record['meta_data']
+    taskname_components = record['task'].split('/')
+
+    def extract_stats_from_step(step):
+
+        acquisitionEra = None
+        for output in step['output']:
+            acquisitionEra = output.get('acquisitionEra')
+            if acquisitionEra is not None:
+                break
+
+        stats = { 'scope': {
+            'workflow': taskname_components[1],
+            'task': taskname_components[-1],
+            'host': meta_data['host'],
+            'site': step['site'],
+            'jobtype': meta_data['jobtype'],
+            'jobstate': meta_data['jobstate'],
+            'step': step['name'],
+            'acquisitionEra': acquisitionEra,
+        } }
+
+        stats['count'] = 1
+
+        stats['performance'] = {
+            'cpu': {
+                'jobCPU': step.get('performance', {}).get('cpu', {}).get('TotalJobCPU'),
+                'jobTime': step.get('performance', {}).get('cpu', {}).get('TotalJobTime'),
+            },
+            'storage': {
+                'read': step.get('performance', {}).get('storage', {}).get('readTotalMB'),
+                'write': step.get('performance', {}).get('storage', {}).get('writeTotalMB'),
+            }
+        }
+
+        return stats
+
+    return map(extract_stats_from_step, record['steps'])
+
+
+def aggregate_stats(stats, existing):
+    if existing is None:
+        return stats
+
+    stats['count'] += existing['count']
+
+    def get_nested(target, path):
+        nested = target
+        for key in path:
+            if nested is None:
+                return None
+            nested = nested.get(key)
+        return nested
+
+    def set_nested(target, path, value):
+        nested = target
+        for key in path[:-1]:
+            nested = nested.setdefault(key, {})
+        nested[path[-1]] = value
+
+    def aggregate_average(path):
+        path_N = path
+        path_N[-1] += "_N"
+        value = get_nested(stats, path)
+        N = get_nested(stats, path_N) or 1
+        existing_value = get_nested(existing, path)
+        existing_N = get_nested(existing, path_N) or 1
+        if value is None:
+            value = existing_value
+            N = existing_N
+        elif existing_value is not None:
+            value = (value * N + existing_value * existing_N) / (N + existing_N)
+            N = N + existing_N
+        set_nested(stats, path, existing_value)
+        set_nested(stats, path_N, existing_N)
+
+    aggregate_average([ 'performance', 'cpu', 'jobCPU' ])
+    aggregate_average([ 'performance', 'cpu', 'jobTime' ])
+    aggregate_average([ 'performance', 'storage', 'read' ])
+    aggregate_average([ 'performance', 'storage', 'write' ])
+
+    return stats
+
+
 class MapReduce(object):
     def __init__(self, spec=None):
         # spec here is redundant since our mapper and reducer does not use it
         self.spec = spec
+        # Timing
+        print("Starting FWJR aggregation...")
+        self.start_time = time.time()
+
 
     def mapper(self, records):
         """
@@ -36,7 +127,6 @@ class MapReduce(object):
                 continue
 
             meta_data = record['meta_data']
-            taskname_components = record['task'].split('/')
 
             # Determine timeframe of aggregation
             timestamp = datetime.fromtimestamp(meta_data['ts'])
@@ -45,63 +135,16 @@ class MapReduce(object):
             if not 'end_date' in document or timestamp > document['end_date']:
                 document['end_date'] = timestamp
 
-            # Treat every step as a separate job(?)
-            for step in record['steps']:
+            # Extract list of stats from record, generally one per step
+            stats_list = extract_stats(record)
 
-                # Define scope
-                scope = {
-                    'workflow': taskname_components[1],
-                    'task': taskname_components[-1],
-                    'host': meta_data['host'],
-                    'site': step['site'],
-                    'jobtype': meta_data['jobtype'],
-                    'jobstate': meta_data['jobstate'],
-                    'step': step['name'],
-                }
-                scope_hash = get_scope_hash(scope)
-
-                # Retrieve existing stats for this scope
-                stats = document['stats'].get(scope_hash)
-                if not stats:
-                    stats = {
-                        'scope': scope,
-                    }
-
-
-                # Aggregate metrics
-
-                # Count
-                if not stats.get('count'):
-                    stats['count'] = 0
-                stats['count'] += 1
-
-                # Performance
-                if not stats.get('performance'):
-                    stats['performance'] = {
-                        "cpu": {
-                            "TotalJobCPU": 0,
-                            "TotalJobTime": 0,
-                        },
-                        "storage": {
-                            "readTotalMB": 0,
-                            "writeTotalMB": 0,
-                        }
-                    }
-                performance_cpu = step['performance']['cpu']
-                if performance_cpu.get('TotalJobCPU', 0):
-                    stats['performance']['cpu']['TotalJobCPU'] += performance_cpu.get('TotalJobCPU', 0)
-                if performance_cpu.get('TotalJobTime', 0):
-                    stats['performance']['cpu']['TotalJobTime'] += performance_cpu.get('TotalJobTime', 0)
-                performance_storage = step['performance']['storage']
-                if performance_storage.get('readTotalMB', 0):
-                    stats['performance']['storage']['readTotalMB'] += performance_storage.get('readTotalMB', 0)
-                if performance_storage.get('writeTotalMB', 0):
-                    stats['performance']['storage']['writeTotalMB'] += performance_storage.get('writeTotalMB', 0)
-
-                # Store stats in document
-                document['stats'][scope_hash] = stats
+            # Merge into document
+            for stats in stats_list:
+                scope_hash = get_scope_hash(stats['scope'])
+                document['stats'][scope_hash] = aggregate_stats(stats, existing=document['stats'].get(scope_hash))
 
         return document
+
 
     def reducer(self, records, init=0):
         "Simpler reducer which collects all results from RDD.collect() records"
@@ -115,18 +158,8 @@ class MapReduce(object):
                 document['end_date'] = existing_document['end_date']
 
             for scope_hash, existing_stats in existing_document['stats'].items():
+                document['stats'][scope_hash] = aggregate_stats(existing_stats, existing=document['stats'].get(scope_hash))
 
-                stats = document['stats'].get(scope_hash)
-                if not stats:
-                    stats = existing_stats
-                else:
-                    stats['count'] += existing_stats['count']
-                    stats['performance']['cpu']['TotalJobCPU'] += existing_stats['performance']['cpu']['TotalJobCPU']
-                    stats['performance']['cpu']['TotalJobTime'] += existing_stats['performance']['cpu']['TotalJobTime']
-                    stats['performance']['storage']['readTotalMB'] += existing_stats['performance']['storage']['readTotalMB']
-                    stats['performance']['storage']['writeTotalMB'] += existing_stats['performance']['storage']['writeTotalMB']
-
-                document['stats'][scope_hash] = stats
 
         # Remove the scope hashes and only store a list of metrics, each with their `scope` attribute.
         # This way we can store the data in MongoDB and later filter/aggregate using the `scope`.
@@ -147,5 +180,6 @@ class MapReduce(object):
         daily_collection.insert(stats)
 
         print("Aggregated performance metrics stored in MongoDB database {}.".format(daily_collection))
+        print("--- {} seconds ---".format(time.time() - self.start_time))
 
         return document
