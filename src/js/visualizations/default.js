28458d6bb1be7439cf8ed3dd21e0d4ab6bea0b56
Nils Fischer <n.fischer@viwid.com>
2016-08-17 14:38:34 +0200

    fixed default 1d bar visualization

--- src/js/visualizations/default.js
+++ src/js/visualizations/default.js
@@ -63,63 +63,77 @@ app.visualizationViews['default'] = Backbone.View.extend({
         .range([ 0, 100 ])
         .domain([ 0, max_count ]);
 
-      var stack = d3.stack()
-          .keys(all_jobstates)
-          .order(d3.stackOrderNone)
-          .offset(d3.stackOffsetNone);
-      var stack_data = stack(data.map(function(d) {
-        var r = {
-          label: d.label,
-        };
-        for (var jobstate of all_jobstates) {
-          r[jobstate] = 0;
-        }
-        for (var dd of d.jobstates) {
-          r[dd.jobstate] = dd.count;
-        }
-        return r;
-      }));
-      stack_data = stack_data[0].map(function(col, i) {
-        var new_row = stack_data.map(function(row) {
-          var new_col = row[i];
-          new_col.jobstate = row.key;
-          return new_col;
+      var bar_data = data;
+
+      if (is_stacked) {
+        var stack = d3.stack()
+            .keys(all_jobstates)
+            .order(d3.stackOrderNone)
+            .offset(d3.stackOffsetNone);
+        var stack_data = stack(data.map(function(d) {
+          var r = {
+            label: d.label,
+          };
+          for (var jobstate of all_jobstates) {
+            r[jobstate] = 0;
+          }
+          for (var dd of d.jobstates) {
+            r[dd.jobstate] = dd.count;
+          }
+          return r;
+        }));
+        stack_data = stack_data[0].map(function(col, i) {
+          var new_row = stack_data.map(function(row) {
+            var new_col = row[i];
+            new_col.jobstate = row.key;
+            return new_col;
+          });
+          new_row.label = col.data.label;
+          return new_row;
         });
-        new_row.label = col.data.label;
-        return new_row;
-      });
+        bar_data = stack_data;
+      }
 
       var container = canvas.selectAll('.full-width-container')
-        .data(stack_data)
+        .data(bar_data)
         .enter()
           .append('div').attr('class', 'full-width-container');
 
       var bar = container.append('svg')
         .attr('class', 'chart')
 
-      bar.selectAll('rect')
-        .data(function(d) {
-          return d;
-        })
-        .enter().append('rect')
-          .attr('height', '100%')
-          .attr('x', function(d) {
-            return length(d[0]) + '%';
+      if (is_stacked) {
+        bar.selectAll('rect')
+          .data(function(d) {
+            return d;
           })
-          .attr('y', 0)
-          .attr('width', function(d) {
-            return (length(d[1]) - length(d[0])) + '%';
-          })
-          .attr('fill', function(d) {
-            return color(d.jobstate);
-          })
-          .attr('data-toggle', 'tooltip')
-          .attr('title', function(d) {
-            var job_count = d[1] - d[0];
-            return d.jobstate + ': ' + app.format_jobs(job_count) + ' (' + numeral(job_count / d3.sum(all_jobstates.map(function(jobstate) {
-              return d.data[jobstate];
-            }))).format('0.0%') + ')';
-          });
+          .enter().append('rect')
+            .attr('height', '100%')
+            .attr('x', function(d) {
+              return length(d[0]) + '%';
+            })
+            .attr('y', 0)
+            .attr('width', function(d) {
+              return (length(d[1]) - length(d[0])) + '%';
+            })
+            .attr('fill', function(d) {
+              return color(d.jobstate);
+            })
+            .attr('data-toggle', 'tooltip')
+            .attr('title', function(d) {
+              var job_count = d[1] - d[0];
+              return d.jobstate + ': ' + app.format_jobs(job_count) + ' (' + numeral(job_count / d3.sum(all_jobstates.map(function(jobstate) {
+                return d.data[jobstate];
+              }))).format('0.0%') + ')';
+            });
+        } else {
+          bar.append("rect")
+            .attr('height', '100%')
+            .attr('width', function(d) {
+              return length(d.average) + '%';
+            })
+            .attr('class', 'filled');
+        }
 
         var label = container.append('div')
           .attr('class', 'chart-label')
@@ -130,8 +144,14 @@ app.visualizationViews['default'] = Backbone.View.extend({
           });
         label.append('text')
           .text(function(d) {
-            var job_count = d[d.length - 1][1];
-            return app.format_jobs(job_count) + ' (' + numeral(job_count / total_count).format('0.0%') + ')';
+            var count = 0;
+            if (is_stacked) {
+              count = d[d.length - 1][1];
+              return app.format_jobs(count) + ' (' + numeral(count / total_count).format('0.0%') + ')';
+            } else {
+              count = d.average;
+              return app.format_value(self.metric)(count);
+            }
           });
 
     } else {
@@ -229,6 +249,14 @@ app.visualizationViews['default'] = Backbone.View.extend({
     }
 
     return this;
+  },
+
+  events: {
+    'click a': 'refineFilter',
+  },
+
+  refineFilter: function(event) {
+    app.scope.set(this.axis, event.target.text);
   },
 
 });
