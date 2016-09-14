f50f025daf846c6702b6261b3cf72a6a61a88d01
yuyiGuo <yuyi.guo.usa@gmail.com>
2016-09-14 14:24:05 -0500

    Added a document to define metrics. UI will use it for explanation of the metrics.
    change case to match schema.

---
+++ src/maps/Doc4Metrics.py
@@ -0,0 +1,28 @@
+{
+"storage":{
+    "writeTotalMB": "Total storage layer written data in MB.",
+    "readPercentageOps" : "% operations by the ROOT layer vs storage.",  
+    "readAverageKB": "Read rate of storage layer in KB/operation.",
+    "readTotalMB": "Total storage layer read in MB.", 
+    "readNumOps": "Storage-based read operation",
+    "readCachePercentageOps": "% successful operations by ROOT layer vs storage.", 
+    "readMBSec": "Read rate MB/millisecond.",
+    "readMaxMSec": "The longest read time in millisecond.",
+    "readTotalSecs": "Total time for root-based read in seconds.",
+    "writeTotalSecs": "Total writting time in seconds."
+            },
+"cpu":{   
+    "MinEventTime":"Shortest event wall time in seconds.",
+    "MaxEventTime":"Longest event wall time in seconds",
+    "AvgEventTime":"Average event wall time in seconds.",
+    "EventThroughput":"Num of events per second.",
+    "TotalJobTime":"Job wall time in seconds.",
+    "TotalJobCPU":"Job CPU in seconds.",
+    "TotalLoopCPU":"in seconds."
+},
+"memory":{
+    "PeakValueRss":"MB",
+    "peakValueVsize":"MB"
+}
+
+}
