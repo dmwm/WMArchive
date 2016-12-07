d0655fea22299a2c80c04d7368eb92595f882dea
Valentin Kuznetsov <vkuznet@gmail.com>
2016-12-07 18:52:02 -0500

    Add int/float patterns

--- src/python/WMArchive/Utils/Regexp.py
+++ src/python/WMArchive/Utils/Regexp.py
@@ -22,3 +22,7 @@ PAT_YYYYMMDD = re.compile(r'^20[0-9][0-9][0-1][0-9][0-3][0-9]$')
 PAT_YYYY = re.compile(r'^20[0-9][0-9]$')
 PAT_MM = re.compile(r'^(0[1-9]|1[012])$')
 PAT_DD = re.compile(r'^(0[1-9]|[12][0-9]|3[01])$')
+
+# number patterns
+PAT_FLOAT = re.compile(r'(^[-]?\d+\.\d*$|^\d*\.{1,1}\d+$)')
+PAT_INT = re.compile(r'(^[0-9-]$|^[0-9-][0-9]*$)')
