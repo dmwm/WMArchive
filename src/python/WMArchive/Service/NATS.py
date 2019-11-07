d251f46ce0932466e132f2c2ad0d1bd37dfe6349
Valentin Kuznetsov <vkuznet@gmail.com>
2019-11-07 08:47:17 -0500

    Add NATS python implementation (to avoid potential DNS overload via external publisher calls)

--- src/python/WMArchive/Service/NATS.py
+++ src/python/WMArchive/Service/NATS.py
@@ -18,7 +18,104 @@ import subprocess
 # WMArchive modules
 from WMArchive.Utils.Utils import cms_filter
 
-def nats(subject, msg, server=None, pub=None):
+# tornado modules
+import tornado.ioloop
+import tornado.gen
+
+# NATS modules
+from nats.io.client import Client as NATS
+
+
+@tornado.gen.coroutine
+def nats(server, subject, msg=None):
+    """
+    NATS client implemented via tornado (NATS py2 approach), see
+    https://github.com/nats-io/nats.py2
+    for python3 implementation see
+    https://github.com/nats-io/nats.py
+    """
+    nc = NATS()
+    yield nc.connect(server)
+    if isinstance(msg, list):
+        for item in msg:
+            yield nc.publish(subject, item)
+    else:
+        yield nc.publish(subject, msg)
+    # do not close, since it will not send it over
+    #yield nc.close()
+
+def nats_encoder(doc):
+    "CMS NATS message encoder"
+    keys = sorted(doc.keys())
+    msg = '___'.join(['{}:{}'.format(k, doc[k]) for k in keys])
+    return msg
+
+def nats_decoder(msg):
+    "CMS NATS message decoder"
+    rec = {}
+    for pair in msg.split('___'):
+        arr = pair.split(':')
+        rec.update({arr[0]:arr[1]})
+    return rec
+
+class NATSManager(object):
+    """
+    NATSManager provide python interface to NATS server
+    """
+    def __init__(self, server=None, topics=None, attrs=None, default_topic='cms-wma', stdout=False):
+        self.topics = topics
+        self.server = server
+        self.def_topic = default_topic
+        self.stdout = stdout
+        self.attrs = attrs
+
+    def repr(self):
+        print('NATSManager@{}, server={} topics={} def_topic={} attrs={} stdout={}'.format(self, self.server, self.topics, self.def_topic, self.attrs, self.stdout))
+
+    def publish(self, data):
+        "Publish given set of docs to topics"
+        cms_msgs = []
+        if not self.topics:
+            # we will use input docs to get list of sites
+            sdict = {}
+            for doc in data:
+                for rec in cms_filter(doc, self.attrs):
+                    subject = rec.get('site', '')
+                    if not subject:
+                        continue
+                    msg = nats_encoder(rec)
+                    sdict.setdefault(subject, []).append(msg)
+                    cms_msgs.append(msg)
+            for topic, msgs in sdict.items():
+                self.send(topic, msgs)
+            # always send all messages to default topic
+            self.send(self.def_topic, cms_msgs)
+            return
+        for topic in self.topics:
+            top_msgs = []
+            pat = re.compile(topic)
+            for doc in data:
+                for rec in cms_filter(doc, self.attrs):
+                    msg = nats_encoder(rec)
+                    if msg.find(topic) != -1 or pat.match(msg):
+                        top_msgs.append(msg) # topic specific messages
+                    cms_msgs.append(msg) # cms-wma messages
+            self.send(topic, top_msgs)
+        # always send all messages to default topic
+        self.send(self.def_topic, cms_msgs)
+
+    def send(self, subject, msg):
+        "Call NATS function, user can pass either single message or list of messages"
+        if self.stdout:
+            if isinstance(msg, list):
+                for item in msg:
+                    print("{}: {}".format(subject, item))
+            else:
+                print("{}: {}".format(subject, msg))
+        else:
+            tornado.ioloop.IOLoop.current().run_sync(lambda: nats(self.server, subject, msg))
+
+def nats_pub(subject, msg, server=None, pub=None):
     "NATS publisher via external NATS_PUB tool"
     if not pub:
         pub = os.getenv('NATS_PUB', '')
@@ -38,21 +135,7 @@ def nats(subject, msg, server=None, pub=None):
     proc.wait()
     return proc.returncode
 
-def nats_encoder(doc):
-    "CMS NATS message encoder"
-    keys = sorted(doc.keys())
-    msg = '___'.join(['{}:{}'.format(k, doc[k]) for k in keys])
-    return msg
-
-def nats_decoder(msg):
-    "CMS NATS message decoder"
-    rec = {}
-    for pair in msg.split('___'):
-        arr = pair.split(':')
-        rec.update({arr[0]:arr[1]})
-    return rec
-
-class NATSManager(object):
+class NATSManager_pub(object):
     "NATS manager for WMArchive"
     def __init__(self, server=None, pub=None, topics=None):
         self.topics = topics
@@ -68,9 +151,9 @@ class NATSManager(object):
                     subject = rec.get('site', '')
                     if not subject:
                         continue
-                    nats(subject, nats_encoder(rec), server=self.server, pub=self.pub)
+                    nats_pub(subject, nats_encoder(rec), server=self.server, pub=self.pub)
                     # send to default cms-wma topic too
-                    nats('cms-wma', nats_encoder(rec), server=self.server, pub=self.pub)
+                    nats_pub('cms-wma', nats_encoder(rec), server=self.server, pub=self.pub)
             return
         for topic in self.topics:
             pat = re.compile(topic)
@@ -78,21 +161,31 @@ class NATSManager(object):
                 for rec in cms_filter(doc):
                     msg = nats_encoder(rec)
                     if msg.find(topic) != -1 or pat.match(msg):
-                        nats(topic, nats_encoder(rec), server=self.server, pub=self.pub)
+                        nats_pub(topic, nats_encoder(rec), server=self.server, pub=self.pub)
                     # send to cms-wma by default
-                    nats('cms-wma', nats_encoder(rec), server=self.server, pub=self.pub)
+                    nats_pub('cms-wma', nats_encoder(rec), server=self.server, pub=self.pub)
 
 def test():
-    "Main function"
-    subject = 'cms-test'
+    "Test function"
+    subject = 'cms-wma'
     msg = 'test from python'
-#     res = nats(subject, msg)
-#     print("return code", res)
     doc = {'site':'1', 'attr':'1'}
     msg = nats_encoder(doc)
     print(msg)
     rec = nats_decoder(msg)
     print(doc, rec)
+
+    data = [{'site':'Site_TEST', 'attr':str(i), 'task':'task-%s'%i} for i in range(10)]
+    server = 'nats://test.host.com'
+    attrs = ['site', 'attr', 'task']
+    mgr = NATSManager(server, attrs=attrs, stdout=True)
+    print("Test NATSManager", mgr)
+    mgr.publish(data)
+    # create new manager with topics
+    topics = ['attr']
+    mgr = NATSManager(server, topics=topics, attrs=attrs, stdout=True)
+    print("Test NATSManager", mgr)
+    mgr.publish(data)
 
 if __name__ == '__main__':
     test()
