d25422cc46b3ac98fb07397a000e4356659655a4
Valentin Kuznetsov <vkuznet@gmail.com>
2023-08-29 09:04:23 -0400

    Use endPoints in tests

--- src/go/wmarchive_test.go
+++ src/go/wmarchive_test.go
@@ -11,24 +11,28 @@ import (
 
 // TestGETDataHandler provides test of GET method for our service
 func TestGETDataHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/wmarchive/data", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	endPoints := []string{"/wmarchive/data", "/wmarchive/data/"}
+	Config.Verbose = 1
+	for _, endPoint := range endPoints {
+		req, err := http.NewRequest("GET", endPoint, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(DataHandler)
-	handler.ServeHTTP(rr, req)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(DataHandler)
+		handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
 
-	expected := info()
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		expected := info()
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
 	}
 }
 
@@ -39,43 +43,47 @@ func TestPOSTDataHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 	fname := fmt.Sprintf("%s/doc.json", dir)
-	file, err := os.Open(fname)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer file.Close()
 
-	req, err := http.NewRequest("POST", "/wmarchive/data", file)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(DataHandler)
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-	// {"result":[{"ids":["74193fb2a07d4302a1f14257454e4b53"],"status":"ok"}]}
-	var rec map[string][]map[string]interface{}
-	//     var rec TestRecord
-	err = json.Unmarshal([]byte(rr.Body.String()), &rec)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if vrec, ok := rec["result"]; ok {
-		for _, r := range vrec {
-			if v, ok := r["status"]; ok {
-				if v.(string) != "ok" {
-					t.Fatal("wmarchive record status is not ok")
-				}
-			} else {
-				t.Fatal("wmarchive record does not have status field")
-			}
+	endPoints := []string{"/wmarchive/data", "/wmarchive/data/"}
+	Config.Verbose = 1
+	for _, endPoint := range endPoints {
+		file, err := os.Open(fname)
+		if err != nil {
+			t.Fatal(err)
 		}
-	} else {
-		t.Fatal("unable to read wmarchive record")
+		defer file.Close()
+		req, err := http.NewRequest("POST", endPoint, file)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(DataHandler)
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+		// {"result":[{"ids":["74193fb2a07d4302a1f14257454e4b53"],"status":"ok"}]}
+		var rec map[string][]map[string]interface{}
+		//     var rec TestRecord
+		err = json.Unmarshal([]byte(rr.Body.String()), &rec)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if vrec, ok := rec["result"]; ok {
+			for _, r := range vrec {
+				if v, ok := r["status"]; ok {
+					if v.(string) != "ok" {
+						t.Fatal("wmarchive record status is not ok")
+					}
+				} else {
+					t.Fatal("wmarchive record does not have status field")
+				}
+			}
+		} else {
+			t.Fatal("unable to read wmarchive record")
+		}
 	}
 }
